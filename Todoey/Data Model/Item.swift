46ca7be3291869a4c4e7acbe4b45ecdf7a82177c
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-12-19 10:40:09 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tanin on 19/12/2017.
+//  Copyright © 2017 landtanin. All rights reserved.
+//
+
+import Foundation
+class Item{
+    
+    var title : String = ""
+    var done: Bool = false
+    
+}
