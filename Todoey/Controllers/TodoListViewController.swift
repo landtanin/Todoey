1b247a31ab7dab695b4d50da95d7e55e9c5e0071
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-12-26 09:26:47 +0000

    Updating Data with Realm (U in CRUD)

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -57,6 +57,23 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //        print(itemArray[indexPath.row])
         
+        if let item = todoItems?[indexPath.row]{
+            
+            do {
+                
+                try realm.write {
+                    item.done = !item.done
+                }
+                
+            } catch {
+                
+                print("Error saving done status, \(error)")
+                
+            }
+        }
+        
+        tableView.reloadData()
+        
 //        todoItems[indexPath.row].done = itemArray[indexPath.row].done
 //
 //        saveItems()
